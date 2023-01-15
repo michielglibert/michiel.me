7407d82fbe1a7d8d59a68b9c1cd947236edb1c65
Michiel Glibert <michiel.glibert@gmail.com>
2023-01-15 22:18:20 +0100

    Test out new portfolio layout

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,27 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import { Inter } from '@next/font/google'
-import styles from '../styles/Home.module.css'
+import Head from "next/head";
+import { Manrope } from "@next/font/google";
+import {
+  Box,
+  Center,
+  GridItem,
+  SimpleGrid,
+  Text,
+  useBreakpoint,
+  useBreakpointValue,
+} from "@chakra-ui/react";
+import { Typewriter } from "react-simple-typewriter";
+import Image from "next/image";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Manrope({ subsets: ["latin"] });
 
 export default function Home() {
+  const shouldRenderBreak = useBreakpointValue({
+    base: "true",
+    lg: false,
+    xl: true,
+    "2xl": false,
+  });
+
   return (
     <>
       <Head>
@@ -14,110 +30,105 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className={styles.main}>
-        <div className={styles.description}>
-          <p>
-            Get started by editing&nbsp;
-            <code className={styles.code}>pages/index.tsx</code>
-          </p>
-          <div>
-            <a
-              href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-              target="_blank"
-              rel="noopener noreferrer"
+      <main className={inter.className}>
+        <Center
+          p="8"
+          fontSize="5xl"
+          fontWeight="bold"
+          h="100vh"
+          bg="radial-gradient(88.82% 88.82% at 6.6% 101.66%, #348AA7 0%, #102C4B 51.47%, #000020 99.52%)"
+          pos="relative"
+        >
+          <SimpleGrid
+            columns={12}
+            w="100%"
+            h="100%"
+            columnGap={{ base: "0", xl: "16" }}
+          >
+            <GridItem
+              gridColumn="span 5"
+              alignSelf="flex-end"
+              justifySelf="flex-end"
+              zIndex={1}
+              display={{ base: "none", xl: "block" }}
             >
-              By{' '}
               <Image
-                src="/vercel.svg"
-                alt="Vercel Logo"
-                className={styles.vercelLogo}
-                width={100}
-                height={24}
-                priority
+                src="/images/3d-male.png"
+                width="440"
+                height="200"
+                alt="3D male"
               />
-            </a>
-          </div>
-        </div>
+            </GridItem>
 
-        <div className={styles.center}>
-          <Image
-            className={styles.logo}
-            src="/next.svg"
-            alt="Next.js Logo"
-            width={180}
-            height={37}
-            priority
+            <GridItem
+              gridColumn={{ base: "span 12", xl: "span 7" }}
+              alignSelf="flex-end"
+              justifySelf={{ base: "center", xl: "flex-start" }}
+              h="690"
+              pt="24"
+            >
+              <Text align={{ base: "center", xl: "left" }} color="white">
+                Hi, I&apos;m Michiel,{shouldRenderBreak && <br />} a{" "}
+                <Box as="span" color="#348AA7">
+                  <Typewriter
+                    words={[
+                      "Software Engineer",
+                      "Problem Solver",
+                      "Tech Enthousiast",
+                      "Team player",
+                    ]}
+                    cursor
+                    cursorColor="#348AA7"
+                    delaySpeed={2500}
+                    typeSpeed={45}
+                    deleteSpeed={45}
+                    loop={0}
+                  />
+                </Box>
+              </Text>
+            </GridItem>
+          </SimpleGrid>
+
+          <Box
+            bottom="0"
+            h="20"
+            w="140%"
+            pos="absolute"
+            borderRadius="50% / 100%;"
+            borderBottomLeftRadius="0"
+            borderBottomRightRadius="0"
+            bg="white"
           />
-          <div className={styles.thirteen}>
-            <Image
-              src="/thirteen.svg"
-              alt="13"
-              width={40}
-              height={31}
-              priority
-            />
-          </div>
-        </div>
-
-        <div className={styles.grid}>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Docs <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Find in-depth information about Next.js features and&nbsp;API.
-            </p>
-          </a>
-
-          <a
-            href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Learn <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Learn about Next.js in an interactive course with&nbsp;quizzes!
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Templates <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Discover and deploy boilerplate example Next.js&nbsp;projects.
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Deploy <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Instantly deploy your Next.js site to a shareable URL
-              with&nbsp;Vercel.
-            </p>
-          </a>
-        </div>
+        </Center>
+        <Center h="100vh" bg="white"></Center>
+        <Center
+          pos="relative"
+          h="200vh"
+          bg="radial-gradient(87.72% 153.49% at 97.34% 2.85%, #348AA7 0%, #102C4B 51.47%, #000020 99.52%)"
+        >
+          <Box
+            top="0"
+            h="20"
+            w="140%"
+            pos="absolute"
+            borderRadius="50% / 100%;"
+            borderTopLeftRadius="0"
+            borderTopRightRadius="0"
+            bg="white"
+          />
+          <Box
+            bottom="0"
+            h="20"
+            w="140%"
+            pos="absolute"
+            borderRadius="50% / 100%;"
+            borderBottomLeftRadius="0"
+            borderBottomRightRadius="0"
+            bg="white"
+          />
+        </Center>
+        <Center h="100vh" bg="white"></Center>
       </main>
     </>
-  )
+  );
 }
